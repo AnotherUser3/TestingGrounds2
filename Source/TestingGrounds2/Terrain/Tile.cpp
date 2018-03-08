817c2e86b412c938f6b70b29a94c2ee6656191cf
Provo Salick <provosalick@live.com>
2018-03-08 14:19:41 -0800

    Lecture 295

--- Source/TestingGrounds2/Terrain/Tile.cpp
+++ Source/TestingGrounds2/Terrain/Tile.cpp
@@ -12,29 +12,50 @@ ATile::ATile()
 
 }
 
-void ATile::PlaceActors(TSubclassOf<AActor> ToSpawn, int MinSpawn, int MaxSpawn)
+void ATile::PlaceActors(TSubclassOf<AActor> ToSpawn, int MinSpawn, int MaxSpawn, float radius)
 {
-	FVector Min(0, -1900, 30);
-	FVector Max(3700, 1900, 30);
-	FBox Bounds(Min, Max);
 	int NumberToSpawn = FMath::RandRange(MinSpawn, MaxSpawn);
 	for (size_t i = 0; i < NumberToSpawn; i++)
 	{
-		FVector SpawnPoint = FMath::RandPointInBox(Bounds);
+		FVector SpawnPoint;
+		bool foundLocation = FindEmptyLocation(SpawnPoint, radius);
+		if (foundLocation)
+		{
+			SpawnActor(ToSpawn, SpawnPoint);
+		}
+	}
+
+}
+bool ATile::FindEmptyLocation(FVector& OutLocation, float radius)
+{
+	FVector Min(0, -2000, 30);
+	FVector Max(4000, 2000, 30);
+	FBox Bounds(Min, Max);
+
+	const int MAX_ATTEMPTS = 100;
+	for (size_t i = 0; i < MAX_ATTEMPTS; i++)
+	{
+		FVector CandidatePoint = FMath::RandPointInBox(Bounds);
+		if (CanSpawnAtLocation(CandidatePoint, radius))
+		{
+			OutLocation = CandidatePoint;
+			return true;
+		}
+	}
+
+	return false;
+}
+void ATile::SpawnActor(TSubclassOf<AActor> ToSpawn, FVector SpawnPoint)
+{
 		AActor* SpawnedActor = GetWorld()->SpawnActor<AActor>(ToSpawn);
 		SpawnedActor->SetActorRelativeLocation(SpawnPoint);
 		SpawnedActor->AttachToActor(this, FAttachmentTransformRules(EAttachmentRule::KeepRelative, false));
-	}
 }
 
 // Called when the game starts or when spawned
 void ATile::BeginPlay()
 {
 	Super::BeginPlay();
-
-	CastSphere(GetActorLocation(), 300);
-	CastSphere(GetActorLocation() + FVector(0,0,1000), 300);
-	CastSphere(GetActorLocation() + FVector(500, 0, 0), 300);
 	
 }
 
@@ -45,13 +66,14 @@ void ATile::Tick(float DeltaTime)
 
 }
 
-bool ATile::CastSphere(FVector Location, float Radius)
+bool ATile::CanSpawnAtLocation(FVector Location, float Radius)
 {
+	FVector GlobalLocation = ActorToWorld().TransformPosition(Location);
 	FHitResult HitResult;
 	bool HasHit = GetWorld()->SweepSingleByChannel(
 		HitResult,
-		Location,
-		Location,
+		GlobalLocation,
+		GlobalLocation,
 		FQuat::Identity,
 		ECollisionChannel::ECC_GameTraceChannel2,
 		FCollisionShape::MakeSphere(Radius)
@@ -60,13 +82,13 @@ bool ATile::CastSphere(FVector Location, float Radius)
 	FColor ResultColor = HasHit ? FColor::Red : FColor::Green;
 		DrawDebugSphere(
 			GetWorld(),
-			Location,
+			GlobalLocation,
 			Radius,
-			32,
+			4,
 			ResultColor,
 			true
 		);
 
 
-	return HasHit;
+	return !HasHit;
 }
--- Source/TestingGrounds2/Terrain/Tile.h
+++ Source/TestingGrounds2/Terrain/Tile.h
@@ -16,7 +16,7 @@ public:
 	ATile();
 
 	UFUNCTION(BlueprintCallable, Category = "Weapon")
-		void PlaceActors(TSubclassOf<AActor> ToSpawn, int MinSpawn, int MaxSpawn);
+		void PlaceActors(TSubclassOf<AActor> ToSpawn, int MinSpawn = 0, int MaxSpawn = 1, float radius = 500);
 
 protected:
 	// Called when the game starts or when spawned
@@ -28,7 +28,8 @@ public:
 
 private:
 
-	bool CastSphere(FVector Location, float Radius);
-	
+	bool CanSpawnAtLocation(FVector Location, float Radius);
+	bool FindEmptyLocation(FVector& OutLocation, float radius);
+	void SpawnActor(TSubclassOf<AActor> ToSpawn, FVector SpawnPoint);
 	
 };
