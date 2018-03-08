f64d6ff11d8538f0e6e99bde9a625651cf212123
Provo Salick <provosalick@live.com>
2018-03-08 11:35:25 -0800

    Lecture 293

--- Source/TestingGrounds2/TestingGrounds2GameMode.cpp
+++ Source/TestingGrounds2/TestingGrounds2GameMode.cpp
@@ -9,8 +9,8 @@ ATestingGrounds2GameMode::ATestingGrounds2GameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Dynamic/Player/Behavior/FirstPersonCharacter"));
-	DefaultPawnClass = PlayerPawnClassFinder.Class;
+	//static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Dynamic/Player/Behavior/FirstPersonCharacter"));
+	//DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
 	//HUDClass = ATestingGrounds2HUD::StaticClass();
